34c818cbec991340287dab29d34f86f700a9a589
Edenio Oliveira <edenio_cdi@yahoo.com.br>
2021-12-28 07:57:30 -0300

    first commit

---
+++ src/br/com/jogoxadres/application/Program.java
@@ -0,0 +1,11 @@
+package br.com.jogoxadres.application;
+
+public class Program {
+
+	public static void main(String[] args) {
+			
+		System.out.println("Uitilizando metodos aprendidos");
+
+	}
+
+}
