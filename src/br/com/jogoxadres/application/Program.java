0479d788303202a51cccd3a264bd461d3d9d6440
Edenio Oliveira <edenio_cdi@yahoo.com.br>
2022-01-04 09:58:02 -0300

    Chess layer and printing the board

--- src/br/com/jogoxadres/application/Program.java
+++ src/br/com/jogoxadres/application/Program.java
@@ -1,13 +1,14 @@
 package br.com.jogoxadres.application;
 
-import br.com.jogoxadres.boardgame.Board;
-import br.com.jogoxadres.boardgame.Position;
+import chess.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
 			
-		Board board = new Board(8, 8);
+		ChessMatch chessMatch = new ChessMatch();
+		
+		UI.printBoard(chessMatch.getPieces());
 
 	}
 
